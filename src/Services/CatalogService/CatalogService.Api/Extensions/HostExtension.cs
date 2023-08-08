471b2ed5a8b10c7e5e93b1c125acb920d2bdd8a4
hamza kılıç <hamza2358@hotmail.com>
2023-08-08 18:10:53 +0300

    added Extensions to Catalog

---
+++ src/Services/CatalogService/CatalogService.Api/Extensions/HostExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CatalogService.Api.Extensions
+{
+    public static class HostExtension
+    {
+        public static IWebHost MigrateDbContext<TContext>(this IWebHost host, Action<TContext, IServiceProvider> seeder) where TContext : DbContext
+        {
+            return 0;
+        }
+    }
+}
